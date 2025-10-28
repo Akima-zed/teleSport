0104ded2c88c093abb057ab6bc465fff4e53bc82
julie <julie.regereau@gmail.com>
2025-10-28 08:58:57 +0100

    commit : ajout commentaire dans le service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { CountryComponent } from './pages/country/country.component';
-import { CountryCard } from ''
+
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, NotFoundComponent, CountryComponent],
--- src/app/services/olympic.service.ts
+++ src/app/services/olympic.service.ts
@@ -12,20 +12,34 @@ export class OlympicService {
 
   constructor(private http: HttpClient) {}
 
+
+  /**
+   *Je récupère toute la liste des pays
+   */
   getCountries(): Observable<Country[]> {
     return this.http.get<Country[]>(this.olympicUrl);
   }
 
+
+  /**
+   *Je récupère un pays par son nom
+   */
   getCountryByName(name: string): Observable<Country | undefined> {
     return this.getCountries().pipe(
       map(countries => countries.find(c => c.country === name))
     );
   }
 
+  /**
+  * Calcule le total des médailles pour un pays donné.
+  */
   getTotalMedals(participations: Participation[]): number {
     return participations.reduce((total, p) => total + p.medalsCount, 0);
   }
 
+  /**
+  * Calcule le total des athlètes pour un pays donné.
+  */
   getTotalAthletes(participations: Participation[]): number {
     return participations.reduce((total, p) => total + p.athleteCount, 0);
   }
